8ea3567a218456d31922f548a85d1765e7e1013d
charlie reyes <charliereyes1228@gmail.com>
2022-10-05 19:02:41 -0700

    added edit and delete button to show page

--- controllers/places.js
+++ controllers/places.js
@@ -54,7 +54,7 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
+    res.render('places/show', { place: places[id],id})
 
   }
 })
