30075dc523badb3074b03f14b888a7bbfd5e60f9
charlie reyes <charliereyes1228@gmail.com>
2022-10-14 17:31:51 -0700

    star rating'

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,9 +4,32 @@ const Def = require('../default')
 function show (data) {
     let comments = (
         <h3 className='inactive'>
-            
+        No comments yet!
         </h3>
     )
+    let rating = (
+    <h3 className="inactive">
+        No yet rated!
+    </h3>
+    )
+    if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+          return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+          stars += 'â­ï¸'
+        }
+      
+        rating = (
+          <h3>
+            {stars} stars
+          </h3>
+        )
+        
+      }
+      
     if (data.place.comments.length) {
         comments = data.place.comments.map(c => {
             return (
@@ -38,6 +61,7 @@ function show (data) {
                         <h2>
                             Rating
                         </h2>
+                        {rating}
                         <br />
                         <h2>
                             Description
